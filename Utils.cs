4b6801edab55e6153a5354b3b1f1fc389b7c042e
hateganfloringeorge <hateganfloringeorge@gmail.com>
2023-12-04 17:13:11 +0200

    Added utils and usings

---
+++ Utils.cs
@@ -0,0 +1,10 @@
+﻿namespace AdventOfCode2023;
+
+public static class Utils
+{
+    public static List<string> ReadFile(string path)
+    {
+        path = Directory.GetParent(Directory.GetCurrentDirectory())?.Parent?.Parent + path;
+        return File.ReadAllLines(path).ToList();
+    }
+}
