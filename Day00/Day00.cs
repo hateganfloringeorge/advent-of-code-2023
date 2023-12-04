0557d31454d6f87608384b5d81cc5f2ec706a031
hateganfloringeorge <hateganfloringeorge@gmail.com>
2023-12-04 21:08:11 +0200

    Added template file

---
+++ Day00/Day00.cs
@@ -0,0 +1,18 @@
+﻿namespace AdventOfCode2023.Day00;
+
+/// <summary>
+/// Template file
+/// </summary>
+public static class Day00
+{
+    private static readonly List<string> input = Utils.ReadFile("/Day00/example01.txt");
+    public static void PartOne()
+    {
+        Console.WriteLine("Part1");
+    }
+
+    public static void PartTwo()
+    {
+        Console.WriteLine("Part2");
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿
+﻿// See https://aka.ms/new-console-template for more information
+
+using AdventOfCode2023.Day00;
+
 Console.WriteLine("Hello, World!");
+Day00.PartOne();
