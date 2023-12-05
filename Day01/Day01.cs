e8225c8628b22d945e3979b0392bed8d34c9a5f2
hateganfloringeorge <hateganfloringeorge@gmail.com>
2023-12-05 13:24:06 +0200

    Removed block scoped namespaces

--- Day01/Day01.cs
+++ Day01/Day01.cs
@@ -1,52 +1,51 @@
-﻿namespace AdventOfCode2023.Day01
-{
-    public static class Day01
-    {
-        private static readonly List<string> input = Utils.ReadFile("/Day01/Day01.txt");
-        public static void PartOne()
-        {
-            Console.WriteLine("======    Day01    ======");
-            var result = 0;
-            foreach (var line in input)
-            {
-                var matches = Regex.Matches(line, "[0-9]");
-                if (matches.Count > 0)
-                {
-                    result += Convert.ToInt32(matches[0].Value + matches[matches.Count - 1].Value);
-                }
-                else
-                {
-                    Console.WriteLine("Something went wrong, no number found");
-                }
-            }
-            Console.WriteLine($"Part1: {result}");
-        }
+﻿namespace AdventOfCode2023.Day01;
 
-        public static void PartTwo()
+public static class Day01
+{
+    private static readonly List<string> input = Utils.ReadFile("/Day01/Day01.txt");
+    public static void PartOne()
+    {
+        Console.WriteLine("======    Day01    ======");
+        var result = 0;
+        foreach (var line in input)
         {
-            var result = 0;
-            foreach (var line in input)
+            var matches = Regex.Matches(line, "[0-9]");
+            if (matches.Count > 0)
             {
-                var updatedLine = line.Replace("one", "o1e")
-                                      .Replace("two", "t2o")
-                                      .Replace("three", "t3e")
-                                      .Replace("four", "f4r")
-                                      .Replace("five", "f5e")
-                                      .Replace("six", "s6x")
-                                      .Replace("seven", "s7n")
-                                      .Replace("eight", "e8t")
-                                      .Replace("nine", "n9e");
-                var matches = Regex.Matches(updatedLine, "[0-9]");
-                if (matches.Count > 0)
-                {
-                    result += Convert.ToInt32(matches[0].Value + matches[matches.Count - 1].Value);
-                }
-                else
-                {
-                    Console.WriteLine("Something went wrong, no number found");
-                }
+                result += Convert.ToInt32(matches[0].Value + matches[matches.Count - 1].Value);
             }
-            Console.WriteLine($"Part2: {result}");
+            else
+            {
+                Console.WriteLine("Something went wrong, no number found");
+            }
         }
+        Console.WriteLine($"Part1: {result}");
+    }
+
+    public static void PartTwo()
+    {
+        var result = 0;
+        foreach (var line in input)
+        {
+            var updatedLine = line.Replace("one", "o1e")
+                                  .Replace("two", "t2o")
+                                  .Replace("three", "t3e")
+                                  .Replace("four", "f4r")
+                                  .Replace("five", "f5e")
+                                  .Replace("six", "s6x")
+                                  .Replace("seven", "s7n")
+                                  .Replace("eight", "e8t")
+                                  .Replace("nine", "n9e");
+            var matches = Regex.Matches(updatedLine, "[0-9]");
+            if (matches.Count > 0)
+            {
+                result += Convert.ToInt32(matches[0].Value + matches[matches.Count - 1].Value);
+            }
+            else
+            {
+                Console.WriteLine("Something went wrong, no number found");
+            }
+        }
+        Console.WriteLine($"Part2: {result}");
     }
 }
--- Day02/Day02.cs
+++ Day02/Day02.cs
@@ -1,57 +1,55 @@
-﻿namespace AdventOfCode2023.Day02
+﻿namespace AdventOfCode2023.Day02;
+
+public static class Day02
 {
-
-    public static class Day02
+    private const string inputFileName = "Day02.txt";
+    private static readonly List<string> input = Utils.ReadFile($"/Day02/{inputFileName}");
+    public static void PartOne()
     {
-        private const string inputFileName = "Day02.txt";
-        private static readonly List<string> input = Utils.ReadFile($"/Day02/{inputFileName}");
-        public static void PartOne()
+        Console.WriteLine("======    Day02    ======");
+        var result = 0;
+        foreach (var line in input)
         {
-            Console.WriteLine("======    Day02    ======");
-            var result = 0;
-            foreach (var line in input)
+            var gameID = Convert.ToInt32(line.Split(':')[0].Split(' ')[1]);
+            bool isValidGame = true;
+            foreach (var cubesShown in line.Split(':')[1].Split(';'))
             {
-                var gameID = Convert.ToInt32(line.Split(':')[0].Split(' ')[1]);
-                bool isValidGame = true;
-                foreach (var cubesShown in line.Split(':')[1].Split(';'))
+                if (!isValidGame) break;
+                foreach (var cubeInfo  in cubesShown.Split(','))
                 {
-                    if (!isValidGame) break;
-                    foreach (var cubeInfo  in cubesShown.Split(','))
+                    // 0 is the empty space before ,;:
+                    var number = Convert.ToInt32(cubeInfo.Split(" ")[1]);
+                    var colour = cubeInfo.Split(" ")[2];
+                    
+                    var max_number = 14;
+                    if (colour.Equals("green"))
                     {
-                        // 0 is the empty space before ,;:
-                        var number = Convert.ToInt32(cubeInfo.Split(" ")[1]);
-                        var colour = cubeInfo.Split(" ")[2];
-                        
-                        var max_number = 14;
-                        if (colour.Equals("green"))
-                        {
-                            max_number = 13;
-                        } 
-                        else if (colour.Equals("red"))
-                        {
-                            max_number = 12;
-                        }
+                        max_number = 13;
+                    } 
+                    else if (colour.Equals("red"))
+                    {
+                        max_number = 12;
+                    }
 
-                        if (max_number < number)
-                        {
-                            isValidGame = false;
-                            break;
-                        }
+                    if (max_number < number)
+                    {
+                        isValidGame = false;
+                        break;
                     }
                 }
-
-                if (isValidGame)
-                {
-                    result += gameID;
-                }
             }
-            Console.WriteLine($"Part1: {result}");
-        }
 
-        public static void PartTwo()
-        {
-            var result = 0;
-            Console.WriteLine($"Part2: {result}");
+            if (isValidGame)
+            {
+                result += gameID;
+            }
         }
+        Console.WriteLine($"Part1: {result}");
+    }
+
+    public static void PartTwo()
+    {
+        var result = 0;
+        Console.WriteLine($"Part2: {result}");
     }
 }
