21f4556083f73d99137a4ddb227bc70bbef0acf7
hateganfloringeorge <hateganfloringeorge@gmail.com>
2023-12-18 10:18:55 +0200

    Added Utils to global usings

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -6,3 +6,4 @@ global using System.Text;
 global using System.Threading.Tasks;
 global using System.Text.RegularExpressions;
 global using System.Diagnostics;
+global using static AdventOfCode2023.Utils;
