53d721930d782e4e8867e8bf181114b713fbc0e7
hateganfloringeorge <hateganfloringeorge@gmail.com>
2023-12-18 18:15:29 +0200

    Miscellaneous template updates

--- Day00/Day00.cs
+++ Day00/Day00.cs
@@ -6,11 +6,15 @@
 public static class Day00
 {
     private const string inputFileName = "Day00.txt";
-    private static readonly List<string> input = Utils.ReadFile($"/Day00/{inputFileName}");
+    private static readonly List<string> input = ReadFile($"/Day00/{ConstantValues.EXAMPLE1}");
     public static void PartOne()
     {
         Console.WriteLine("======    Day00    ======");
         var result = 0;
+        foreach (var line in input)
+        {
+            Console.WriteLine(line);
+        }
         Console.WriteLine($"Part1: {result}");
     }
 
